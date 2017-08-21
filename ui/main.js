7407cdddd5d9aa95ce7608cea8bd7e43a1d6a81f
Nishant Kumar <phy.nishant@gmail.com>
2017-08-21 07:42:52 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 console.log('Loaded!');
 //to move the image
 var img = document.getElementById('madi');
-var marginLeft =0;
+var marginLeft = 0;
 function moveRight(){
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft =marginLeft + 'px';
+    marginLeft=marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
     var intervel = setInterval(moveRight , 100);
