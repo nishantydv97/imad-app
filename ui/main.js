350f4eadb669d667847d80aace0a013a710d0fa3
Nishant Kumar <phy.nishant@gmail.com>
2017-08-21 07:40:17 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var img = document.getElementById('madi');
 var marginLeft =0;
 function moveRight(){
     marginLeft=marginLeft + 1;
-    img.styl.marginLeft =marginLeft + 'px';
+    img.style.marginLeft =marginLeft + 'px';
 }
 img.onclick = function(){
     var intervel = setInterval(moveRight , 100);
